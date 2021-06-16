c3df914040538a7762f37a3eedfa78516b0f26f3
jihoon <akdlsz21@gmail.com>
2021-06-16 16:22:30 +0900

    finished

--- script.js
+++ script.js
@@ -1,32 +1,107 @@
+function getRandomColor() {
+	return Math.floor(Math.random() * 255);
+}
 const container = document.querySelector('.container');
-
+const range = document.querySelector('.range');
+let val = range.value;
+let color = false;
+const textColor = document.querySelector('.color-mode');
 // Generates div.grid inside container
-function makeGrid(size) {
-	for (i = 0; i < size; i++) {
-		let grid = document.createElement('div');
-		container.appendChild(grid).className = 'grid';
+function makeGrid(rowNum, columnNum) {
+	deleteContainer();
+
+	for (i = 0; i < rowNum; i++) {
+		let rows = document.createElement('div');
+		container.appendChild(rows).className = 'rows';
+		for (j = 0; j < columnNum; j++) {
+			let columns = document.createElement('div');
+			rows.appendChild(columns).className = 'columns';
+		}
 	}
 }
 
-makeGrid(256);
+makeGrid(val, val);
 
 // grid mouse hover makes grid element node background black
 
-const grid = document.getElementsByClassName('grid');
+function onHover() {
+	const grid = document.querySelectorAll('.columns');
 
-for (i = 0; i < grid.length; i++) {
-	//aEL callback function takes objects as parameter. more on this later
-	grid[i].addEventListener('mouseover', (tar) => {
-		console.log(tar);
-		tar.target.style.background = 'black'; // Needs to be this way, don't know why
-	});
+	for (i = 0; i < grid.length; i++) {
+		grid[i].addEventListener('mouseover', (e) => {
+			if (color == false) {
+				e.target.style.background = 'black';
+			} else {
+				e.target.style.background = `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`;
+			}
+		});
+	}
 }
+
+onHover();
 
 // reset button to set all grid background color to original state
 const resetButton = document.querySelector('.reset');
-
-resetButton.addEventListener('click', () => {
+function resetBoard() {
+	const grid = document.querySelectorAll('.columns');
 	for (i = 0; i < grid.length; i++) {
 		grid[i].style.background = 'white';
 	}
+}
+resetButton.addEventListener('click', resetBoard);
+
+// make range for pixel columns
+
+range.addEventListener('change', () => {
+	val = document.querySelector('.range').value;
+	// console.log(val);
+	makeGrid(val, val);
+	onHover();
+	changeNums();
 });
+
+// delete grid function for input range change
+
+function deleteContainer() {
+	container.innerHTML = '';
+}
+
+// display number of pixels next to range
+function changeNums() {
+	const nums = document.querySelector('.nums');
+	nums.textContent = `${val} x ${val}`;
+}
+
+changeNums();
+
+const toggleBtn = document.querySelector('.colorSwitch');
+console.log(toggleBtn);
+
+function toggleColor() {
+	toggleBtn.addEventListener('click', () => {
+		console.log('dd');
+		color = !color;
+		onHover();
+		resetBoard();
+		textColorFunc();
+	});
+}
+
+toggleColor();
+
+// function to make grid background initially white
+function initWhite() {
+	container.style.background = 'white';
+}
+initWhite();
+
+// function to make switch color indicator
+function textColorFunc() {
+	if (color == true) {
+		textColor.textContent = 'ON';
+		textColor.style.color = 'rgb(245, 87, 87)';
+	} else {
+		textColor.textContent = 'OFF';
+		textColor.style.color = 'black';
+	}
+}
